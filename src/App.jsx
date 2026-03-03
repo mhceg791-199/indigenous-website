32380320249fa3dcc5707b07d65f864a878bf114
mhceg791-199 <mhceg791@gmail.com>
2026-03-03 14:59:34 +0200

    upadated

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import "./App.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "./App.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 
-import Home from "./pages/Home/Home";
-import Layout from "./Layout";
-import About from "./pages/about/about";
+// import Home from "./pages/Home/Home";
+// import Layout from "./Layout";
+// import About from "./pages/about/about";
 // import Projects from "./pages/projects/projects";
-import ProjectsDetails from "./pages/projectsDetails/projectsDetails";
-import ScrollToTopButton from "./components/shared/ScrollToTopButton/ScrollToTopButton";
-import Services from "./pages/Services/Services";
-import Career from "./pages/Career/Career";
-import ContactUs from "./pages/ContactUs/ContactUs";
-import Terms from "./pages/Terms/Terms";
-import Privacy from "./pages/Privacy/Privacy";
+// import ProjectsDetails from "./pages/projectsDetails/projectsDetails";
+// import ScrollToTopButton from "./components/shared/ScrollToTopButton/ScrollToTopButton";
+// import Services from "./pages/Services/Services";
+// import Career from "./pages/Career/Career";
+// import ContactUs from "./pages/ContactUs/ContactUs";
+// import Terms from "./pages/Terms/Terms";
+// import Privacy from "./pages/Privacy/Privacy";
 
 function App() {
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route
           path="/*"
           element={
@@ -28,18 +28,28 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="about" element={<About />} />
                 <Route path="services" element={<Services />} />
-                {/* <Route path="projects" element={<Projects />} /> */}
                 <Route path="projects/:slug" element={<ProjectsDetails />} />
                 <Route path="career" element={<Career />} />
                 <Route path="contact-us" element={<ContactUs />} />
                 <Route path="terms" element={<Terms />} />
-                <Route path="privacy" element={<Privacy />} /> {/**/}
+                <Route path="privacy" element={<Privacy />} />
               </Routes>
             </Layout>
           }
         />
       </Routes>
-      <ScrollToTopButton />
+      <ScrollToTopButton /> */}
+      <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#ffffff",
+      }}
+    >
+      {/* صفحة بيضاء فاضية */}
+    </div>
     </>
   );
 }
